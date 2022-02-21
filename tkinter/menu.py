69ec738f6302ebb59eff6d564c758c00f106202a
anabelberumen <berumen.anabel@gmail.com>
2022-02-21 01:05:42 -0700

    se agregaron ejemplos de alertas

--- tkinter/menu.py
+++ tkinter/menu.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Aplicacion:
     def __init__(self):
@@ -10,9 +11,9 @@ class Aplicacion:
         self.archivoMenu = Menu(self.menuBar, tearoff=0)
         
         self.archivoMenu.add_command(label="Nuevo")
-        self.archivoMenu.add_command(label="Abrir")
-        self.archivoMenu.add_command(label="Guardar")
-        self.archivoMenu.add_command(label="Cerrar")
+        self.archivoMenu.add_command(label="Abrir", command=self.atencion)
+        self.archivoMenu.add_command(label="Guardar", command=self.error)
+        self.archivoMenu.add_command(label="Cerrar", command=self.cerrar)
         self.archivoMenu.add_separator()
         self.archivoMenu.add_command(label="Salir", command=self.root.quit)
 
@@ -23,7 +24,7 @@ class Aplicacion:
         self.editMenu.add_command(label="Pegar")
 
         self.ayudaMenu = Menu(self.menuBar,tearoff=0)
-        self.ayudaMenu.add_command(label="Ayuda")
+        self.ayudaMenu.add_command(label="Licencia", command=self.licencia)
         self.ayudaMenu.add_separator()
 
         self.menuBar.add_cascade(label="Archivo", menu=self.archivoMenu)
@@ -34,6 +35,23 @@ class Aplicacion:
 
 
         self.root.mainloop()
+
+
+    def cerrar(self):
+        messagebox.askquestion("Cerrar",message="¿Esta seguro?")
+    
+
+    def licencia(self):
+        messagebox.showinfo("Versión", message="Versión 1.0")
+
+
+    def error(self):
+        messagebox.showerror("!ERROR CRITICO!", message="SE ENCONTRO UN ERROR FATAL")
+
+    
+    def atencion(self):
+        messagebox.showwarning("ATENCIÓN", message= "Se borrara el actual")
+
 
 
 
